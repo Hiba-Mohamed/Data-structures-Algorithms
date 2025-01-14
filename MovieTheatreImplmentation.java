65835b2b3f182fde245c64d1afe9d3ddccef2f1a
Hiba-Mohamed <hibamoha987@gmail.com>
2025-01-14 13:23:58 -0330

    second assignement instructions added

---
+++ MovieTheatreImplmentation.java
@@ -0,0 +1,9 @@
+// Instructions
+// Simulate a movie theater that helps users to book and reserve seats.
+// reserve seats (if seat is taken, let them know it is taken and suggest an available seat)
+// cancel seats
+// retrieve initial seating charting
+
+public class MovieTheatreImplmentation {
+    
+}
