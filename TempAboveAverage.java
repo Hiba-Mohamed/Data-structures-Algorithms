1e4a6561b488bdb37fdb9451ba36526ead407932
Hiba-Mohamed <hibamoha987@gmail.com>
2025-01-14 13:56:23 -0330

    assignment #1 done

--- TempAboveAverage.java
+++ TempAboveAverage.java
@@ -1,5 +1,42 @@
-// Calculate the avarage temperature, and give how many days are above the avarage temperature
-//   1. Take an input from the user (eg. 5)
-//   2. Prompt the user to enter all the 5 numbers (temperature values)
-//   3. Calculate the avarage temperature
-//   4. Given the avarage temperature, how many of the numbers in the line 2 are above the avarage temperature?
+import java.util.Scanner;
+
+public class TempAboveAverage {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("");
+
+        System.out.print("Please enter how many temperature values you would like to calculate the average of: ");
+        int lengthOfArray = scanner.nextInt();
+
+        double[] temperatures = new double[lengthOfArray];
+
+        for (int i = 0; i < lengthOfArray; i++) {
+            System.out.print("Enter temperature value #" + (i + 1) + ": ");
+            temperatures[i] = scanner.nextDouble();
+        }
+
+        double sumOfAllValues = 0;
+        for (int i = 0; i < lengthOfArray; i++) {
+            sumOfAllValues += temperatures[i];
+        }
+        double averageTemp = sumOfAllValues / lengthOfArray;
+        System.out.println("");
+
+        System.out.println("Average Temperature: " + averageTemp);
+                System.out.println("");
+
+        System.out.println("Temperatures above average:");
+        int countAboveAverage = 0;
+
+        for (int i = 0; i < lengthOfArray; i++) {
+            if (temperatures[i] > averageTemp) {
+                System.out.println("Temperature value #" + (i + 1) + ": " + temperatures[i]);
+                countAboveAverage++;
+            }
+        }
+
+        System.out.println("");
+
+        scanner.close();
+    }
+}
