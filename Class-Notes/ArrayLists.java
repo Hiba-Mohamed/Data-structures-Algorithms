2e35275a58a48d1acea85efe42659c21bec1a4d0
Hiba-Mohamed <hibamoha987@gmail.com>
2025-01-27 11:33:31 -0330

    added instructors note

---
+++ Class-Notes/ArrayLists.java
@@ -0,0 +1,34 @@
+public class ArrayLists {
+    
+}
+// Array list underneath array list is using arrays.
+// But the difference noun is this. With array list you can dynamically add and remove.
+// Space, or what's the name like so you can dynamically resize the array.
+// So you have 10 now, assuming you want to add 11th element, what happens?
+// It will dynamically resize this array.
+// It would add to create.
+// One with 15.
+// So it does add an additional 5 to it which is.
+// So it increases the size by 5. The initial capacity is 10, but if you want to add additional elements then it increases.
+// By 5 a factor of 5.
+// So right now you can copy 1234567891011.
+// So this is the initial capacity, whatever this is, OK.
+// And then it would increase again.
+// So here we come null and all of that null, null, null. So as you keep adding more elements into the array list it just increases by a factor of 5.
+// If you decide to remove.
+// An element or this?
+// It is still going to be the same.
+// You still going to have this until you call a certain method and I'll be showing you.
+// Until you call what we call trim to size.
+// Trim to size, trim to size is a method within the array list.
+// It is only then and then that it will resize the entire space block to match the number of elements.
+// So array list is basically an interface or it's a very great implementation of the array that has a lot of methods that helps you to easily use arrays.
+// OK.
+// So the take away from this particular class is that with array list it has a dynamic size.
+// With arrays it is fixed. That's wonderful.
+// Then the second difference is that with arrays you do have.
+// With array list there is a performance issue.
+// Because it's going to be because you're going to be copying data from one location to the other and then increasing the size.
+// There is a performance overhead. OK, so that is also that for the difference. The third difference is that when you create an.
+// Ari, usually when you create an array you determine the data type you want to store.
+// So even it's an integer. Then once you declare the array, you can only store integers.
